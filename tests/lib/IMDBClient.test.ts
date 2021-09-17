419bd71be0a125f9d6e28220b74396e8e0a623ba
Rovelcio Junior <rovelciojr@gmail.com>
2021-09-17 15:11:26 -0300

    Test for IMDBClient is no longer static

--- tests/lib/IMDBClient.test.ts
+++ tests/lib/IMDBClient.test.ts
@@ -2,16 +2,8 @@ import { Movie } from "../../src/types/data/Movie";
 import { IMDBClient } from "../../src/lib/IMDBClient";
 
 const imdbClient = new IMDBClient("925eba28");
-const batmanBegins = new Movie(
-  "Batman Begins",
-  "2005",
-  "tt0372784",
-  "movie",
-  "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-);
-
 test("IMDB API Client is working to catch Batman", () => {
   return imdbClient.getMovieById("tt0372784").then((testMovie) => {
-    expect(testMovie).toMatchObject(batmanBegins);
+    expect(testMovie).toBeInstanceOf(Movie);
   });
 });
